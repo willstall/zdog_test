e5b483314bc3bcee93a647e68d208ef19458e033
William Stallwood <will@cipherprime.com>
2019-06-25 17:12:24 -0400

    indy hall

--- indy-hall.js
+++ indy-hall.js
@@ -11,6 +11,7 @@ let height = 40;
 let width = 20;
 let depth = 10;
 
+let duration = 3.0;
 let speed = 0.03;
 let isSpinning = true;
 
@@ -48,7 +49,7 @@ let i = new Zdog.Shape({
         { y: -height,z:-depth*0.5 },
         { y:  height,z:depth*0.5 }
     ],
-    translate: {x:-width*1.25},
+    translate: {x:-width*1.75},
     stroke: 20,
     color: colors[0]
 });
@@ -66,22 +67,32 @@ let h = new Zdog.Shape({
         { x: -width, y: 0.0,z:depth*0.5 }
     ],
     closed: false,
-    translate: {x:width*1.25},
+    translate: {x:width},
     stroke: 20,
     color: colors[0]
 });
 
 illo.updateRenderGraph();
 
+let elapsedSeconds = () => performance.now()/1000;
+let fract = (x) => x - Math.floor(x);
+let easeInOutQuad = (t) =>  t<.5 ? 2*t*t : -1+(4-2*t)*t ;
+
 let animate = () =>
 {
+    let time = fract(elapsedSeconds()/duration);
+    let interval = Math.floor(time*2.0);
+    //Zdog.TAU
+    let target = Math.PI*easeInOutQuad(fract(time*2.0)) + Math.PI*interval;
+
+    // console.log(interval);
     if(isSpinning)
     {
-        outline_1.rotate.y += speed;
-        outline_2.rotate.y += speed*0.5;
-        group.rotate.y += speed;
-        i.rotate.y += speed*0.5;
-        h.rotate.y += speed*0.5;
+        outline_1.rotate.y = target;
+        outline_2.rotate.y = target*2.0;
+        group.rotate.y = target;
+        i.rotate.y = target*2.0;
+        h.rotate.y = target*2.0;
     }
     illo.updateRenderGraph();
     requestAnimationFrame( animate );
